d7e0f34105883943e06093292135f01b13c14bee
xijinping-CN <jia_le_liu@icloud.com>
2022-12-13 21:19:41 +0800

    end

--- src/main/java/com/macro/mall/tiny/component/OrderTimeOutCancelTask.java
+++ src/main/java/com/macro/mall/tiny/component/OrderTimeOutCancelTask.java
@@ -16,10 +16,10 @@ public class OrderTimeOutCancelTask {
      * cron表达式：Seconds Minutes Hours DayofMonth Month DayofWeek [Year]
      * 每10分钟扫描一次，扫描设定超时时间之前下的订单，如果没支付则取消该订单
      */
-    @Scheduled(cron = "0/5 * * ? * ?")
+    @Scheduled(cron = "0/20 * * ? * ?")
     private void cancelTimeOutOrder() {
         // TODO: 2019/5/3 此处应调用取消订单的方法，具体查看mall项目源码
-        System.out.println("测试：十秒一次的轮询开始");
+        System.out.println("测试：二十秒一次的轮询开始");
         LOGGER.info("取消订单，并根据sku编号释放锁定库存");
     }
 }
