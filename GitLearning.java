9e31cc21fdd167143cf2b54d88d641ea1c48ce1d
zhangsan <zhangsan@imooc.com>
2023-01-08 15:10:32 +0800

    1 to 23

--- GitLearning.java
+++ GitLearning.java
@@ -1,7 +1,7 @@
 public  class GitLearning{
 	public static void main(String[] args){
 		System.out.println(11);
-		System.out.println(22);
+		System.out.println(23);
 		System.out.println(3);
 		System.out.println(4);
 		System.out.println(5);
